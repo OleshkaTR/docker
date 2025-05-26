b111fae84890354db8335ce239ad2edc364f1d41
Oleh Triukhan <oleh.t@vektortms.com>
2025-05-26 21:44:15 +0300

    Working with docker

--- ui/src/App.js
+++ ui/src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <ul>
       {movies.map(({title, year, director}) => (
-        <li key={title}>{title} {year} - {director}!!!</li>
+        <li key={title}>{title} {year} - {director}</li>
       ))}
     </ul>
   );
